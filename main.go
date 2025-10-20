08f466f456337de6e410da4ed42853656b2e392a
r3g492 <kangggunw3492@gmail.com>
2025-10-20 23:11:34 +0900

    add mouse log

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
 	rl.InitWindow(2000, 1200, "raylib [core] example - basic window")
@@ -10,37 +15,52 @@ func main() {
 
 	cubePos := rl.NewVector3(0.0, 0.0, 0.0)
 
+	now := time.Now()
+
+	logEvery := 1000 * time.Millisecond
+	lastLog := now
+
 	for !rl.WindowShouldClose() {
+		now = time.Now()
+		dt := rl.GetFrameTime()
+		mouseLocation := rl.GetMousePosition()
+
+		// log
+		if time.Since(lastLog) >= logEvery {
+			fmt.Println("mouseLocation:", mouseLocation)
+			fmt.Println("dt:", dt)
+			lastLog = time.Now()
+		}
 
 		isUp := false
 		_ = false
 		isLeft := false
 		_ = false
-		if rl.IsKeyDown(rl.KeyUp) {
+		if rl.IsKeyDown(rl.KeyW) {
 			isUp = true
 		}
-		if rl.IsKeyDown(rl.KeyLeft) {
+		if rl.IsKeyDown(rl.KeyA) {
 			isLeft = true
 		}
-		if rl.IsKeyDown(rl.KeyDown) && !isUp {
+		if rl.IsKeyDown(rl.KeyS) && !isUp {
 			// isDown = true
 		}
-		if rl.IsKeyDown(rl.KeyRight) && !isLeft {
+		if rl.IsKeyDown(rl.KeyD) && !isLeft {
 			// isRight = true
 		}
 
 		// 12369874
 
-		if rl.IsKeyDown(rl.KeyUp) {
+		if rl.IsKeyDown(rl.KeyW) {
 			cubePos.Z -= 0.3
 		}
-		if rl.IsKeyDown(rl.KeyLeft) {
+		if rl.IsKeyDown(rl.KeyA) {
 			cubePos.X -= 0.3
 		}
-		if rl.IsKeyDown(rl.KeyDown) {
+		if rl.IsKeyDown(rl.KeyS) {
 			cubePos.Z += 0.3
 		}
-		if rl.IsKeyDown(rl.KeyRight) {
+		if rl.IsKeyDown(rl.KeyD) {
 			cubePos.X += 0.3
 		}
 
